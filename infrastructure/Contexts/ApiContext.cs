1d31956d01600c68c4a92abec7ef11f934442353
Sneeekie123 <143397337+Sneeekie123@users.noreply.github.com>
2024-04-19 19:27:26 +0200

    Add project files.

---
+++ infrastructure/Contexts/ApiContext.cs
@@ -0,0 +1,11 @@
+﻿using infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace infrastructure.Contexts;
+
+public class ApiContext(DbContextOptions<ApiContext> options) : DbContext(options)
+{
+    public DbSet<SubscribersEntity> Subscribers { get; set; }
+
+    public DbSet<CourseEntity> Courses { get; set; }
+}
